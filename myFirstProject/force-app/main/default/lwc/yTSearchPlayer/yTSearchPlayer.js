d9be556a987facd9e1a53299f142102102e11ede
Arpit Gupta <arpitgupta630@gmail.com>
2022-12-16 19:44:36 +0530

    Youtube Integration

--- myFirstProject/force-app/main/default/lwc/yTSearchPlayer/yTSearchPlayer.js
+++ myFirstProject/force-app/main/default/lwc/yTSearchPlayer/yTSearchPlayer.js
@@ -6,6 +6,10 @@ export default class YTSearchPlayer extends LightningElement {
     @track finalError = '';
     @track searchInput = 'Cyntexa';
     @track videoResults = [];
+    @track allVideos = [];
+    @track currentPage = 0;
+    @track currentDisplayPage = 1;
+    @track totalPage = 0;
     @track viewUrl = '';
 
     connectedCallback(){
@@ -20,19 +24,41 @@ export default class YTSearchPlayer extends LightningElement {
     handleSubmit(){
         getVideos({searchKey:this.searchInput})
         .then ((results)=>{
-            this.videoResults = results;
-            console.log('This is final video results ::'+ JSON.stringify(this.videoResults));
+            let videos = results;
+            let tempList = [[]];
+            let index = 0;
+            for(let i=0; i<videos.length; i++){
+                if(i%5==0 && i!=0){
+                    tempList.push([]);
+                    index++
+                }else{
+                    tempList[index].push(videos[i])
+                }
+            }
+            this.allVideos = [...tempList];
+            this.videoResults = this.allVideos[0];
+            this.totalPage = this.allVideos.length;
+            console.log(tempList);
 
-            if (this.videoResults.length > 0) {
-                this.showVideoInIframe(this.videoResults[0].videoId);
+            for(let i=0; i<this.allVideos.length; i++){
+                this.allVideos[i]['index'] = i+1;
+            }
+
+            if (this.videoResults[0].length > 0) {
+                this.showVideoInIframe(this.videoResults[0][0].videoId);
             }
         })
         .catch((error)=>{
             this.finalError = error.body.message;
             console.log('This is final video results ::'+ this.finalError);
         })
-
+        this.isPlay = false;
     }
+    handleEnter(event){
+        if(event.keyCode === 13){
+          this.handleSubmit();
+        }
+      }
 
     showVideoInIframe(videoId){
         this.viewUrl = 'https://www.youtube.com/embed/'+videoId;
@@ -45,4 +71,46 @@ export default class YTSearchPlayer extends LightningElement {
         console.log('This is selected video:'+ slt);
         this.viewUrl = 'https://www.youtube.com/embed/'+slt;
     }
+
+    videoList = true;
+    handleNext(){
+        this.videoList = false;
+        if(this.currentPage + 1 < this.allVideos.length){
+            this.currentPage = this.currentPage + 1;
+            this.videoResults = [...this.allVideos[this.currentPage]];
+        }
+        this.videoList = true;
+        this.currentDisplayPage = this.currentPage + 1;
+        console.log(this.currentPage);
+        console.log(this.videoResults);
+    }
+    get disableNextBtn(){
+        return this.allVideos.length == this.currentPage+1;
+    }
+
+    handlePrevious(){
+        this.videoList = false;
+        if(this.currentPage-1 > -1){
+            this.currentPage = this.currentPage - 1;
+            this.videoResults = [...this.allVideos[this.currentPage]];
+        }
+        this.videoList = true;
+        this.currentDisplayPage = this.currentPage + 1;
+        console.log(this.currentPage);
+        console.log(this.videoResults);
+    }
+    get disablePreviousBtn(){
+        return this.currentPage == 0
+    }
+
+    handleChange(event){
+        let allBtn = JSON.parse(JSON.stringify(this.template.querySelectorAll('.slds-button')));
+        console.log(allBtn);
+        this.videoList = false;
+        this.currentPage = Number(event.currentTarget.dataset.id);
+        this.videoResults = [...this.allVideos[this.currentPage]];
+        this.currentDisplayPage = this.currentPage + 1;
+        this.videoList = true;
+    }
+
 }
